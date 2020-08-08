7cd1dfededc65f6ffb0bc3ef4800539549f5e84a
Luke Thompson <endeavour9@gmail.com>
2020-08-08 23:15:27 +1000

    add ewoks

---
+++ scripts/ewoks.js
@@ -0,0 +1,20 @@
+import http from 'k6/http';
+import { check } from "k6";
+
+export let options = {
+  stages: [
+      // Ramp-up from 1 to 5 VUs in 5s
+      { duration: "5s", target: 5 },
+
+      // Stay at rest on 5 VUs for 10s
+      { duration: "10s", target: 5 },
+
+      // Ramp-down from 5 to 0 VUs for 5s
+      { duration: "5s", target: 0 }
+  ]
+};
+
+export default function () {
+  const response = http.get("https://swapi.dev/api/people/30/", {headers: {Accepts: "application/json"}});
+  check(response, { "status is 200": (r) => r.status === 200 });
+};
