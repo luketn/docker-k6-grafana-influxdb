0e6e39044aeec5634b0ed6e3126b38dc551f8cf3
Luke Thompson <endeavour9@gmail.com>
2020-08-13 17:50:19 +1000

    add a message with a link to load the browser with grafana

--- run-load-test.sh
+++ run-load-test.sh
@@ -1,3 +1,5 @@
 docker-compose up -d influxdb grafana
+echo "----------------------------------------------------------"
 echo "Load testing with Grafana dashboard http://localhost:3000/"
+echo "----------------------------------------------------------"
 docker-compose run k6 run /scripts/ewoks.js
