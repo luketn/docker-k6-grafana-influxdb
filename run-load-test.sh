ebc271f18589ca4a4b7ee23fef39a0bd51705e20
Luke Thompson <endeavour9@gmail.com>
2020-08-16 10:42:40 +1000

    Update the browser link to go straight to the k6 dashboard

--- run-load-test.sh
+++ run-load-test.sh
@@ -1,5 +1,5 @@
 docker-compose up -d influxdb grafana
-echo "----------------------------------------------------------"
-echo "Load testing with Grafana dashboard http://localhost:3000/"
-echo "----------------------------------------------------------"
+echo "--------------------------------------------------------------------------------------"
+echo "Load testing with Grafana dashboard http://localhost:3000/d/k6/k6-load-testing-results"
+echo "--------------------------------------------------------------------------------------"
 docker-compose run k6 run scripts/ewoks.js
