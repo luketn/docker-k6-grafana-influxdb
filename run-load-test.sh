6efc0a7d178186c7e51df3f0f341f44301f83804
Luke Thompson <endeavour9@gmail.com>
2021-08-29 20:38:18 +1000

    add --rm so that k6 runs will not leave dangling images

--- run-load-test.sh
+++ run-load-test.sh
@@ -2,4 +2,4 @@ docker-compose up -d influxdb grafana
 echo "--------------------------------------------------------------------------------------"
 echo "Load testing with Grafana dashboard http://localhost:3000/d/k6/k6-load-testing-results"
 echo "--------------------------------------------------------------------------------------"
-docker-compose run k6 run /scripts/ewoks.js
+docker-compose run --rm k6 run /scripts/ewoks.js
