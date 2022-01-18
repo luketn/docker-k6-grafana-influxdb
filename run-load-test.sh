5d01d4f4b77a19677fbcb75f57f5f2d473abbbb6
Wenceslao Castillo <josecastillo.globant@latam.com>
2022-01-18 13:14:58 -0300

    update docker-compose call

--- run-load-test.sh
+++ run-load-test.sh
@@ -1,5 +1,5 @@
-docker-compose up -d influxdb grafana
+docker compose up -d influxdb grafana
 echo "--------------------------------------------------------------------------------------"
 echo "Load testing with Grafana dashboard http://localhost:3000/d/k6/k6-load-testing-results"
 echo "--------------------------------------------------------------------------------------"
-docker-compose run --rm k6 run /scripts/ewoks.js
+docker compose run --rm k6 run /scripts/ewoks.js
