88becf1d2a380539bbdd56941493ed1a647f8b10
Luke Thompson <endeavour9@gmail.com>
2022-11-18 09:59:05 +1100

    Support dynamically setting the VUs by environment variable (was a question in one of the article comments)

--- scripts/ewoks.js
+++ scripts/ewoks.js
@@ -1,15 +1,22 @@
 import http from 'k6/http';
 import { check, sleep } from "k6";
 
+const isNumeric = (value) => /^\d+$/.test(value);
+
+const default_vus = 5;
+
+const target_vus_env = `${__ENV.TARGET_VUS}`;
+const target_vus = isNumeric(target_vus_env) ? Number(target_vus_env) : default_vus;
+
 export let options = {
   stages: [
-      // Ramp-up from 1 to 5 virtual users (VUs) in 5s
-      { duration: "5s", target: 5 },
+      // Ramp-up from 1 to TARGET_VUS virtual users (VUs) in 5s
+      { duration: "5s", target: target_vus },
 
-      // Stay at rest on 5 VUs for 10s
-      { duration: "10s", target: 5 },
+      // Stay at rest on TARGET_VUS VUs for 10s
+      { duration: "10s", target: target_vus },
 
-      // Ramp-down from 5 to 0 VUs for 5s
+      // Ramp-down from TARGET_VUS to 0 VUs for 5s
       { duration: "5s", target: 0 }
   ]
 };
