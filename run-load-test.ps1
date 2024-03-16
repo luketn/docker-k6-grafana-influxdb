dbcf061f2d14f045c754386316c96c566e86eb3a
Luke Thompson <endeavour9@gmail.com>
2024-03-16 11:33:43 +1100

    Update run-load-test.ps1

--- run-load-test.ps1
+++ run-load-test.ps1
@@ -1,5 +1,5 @@
-docker-compose up -d influxdb grafana
+docker compose up -d influxdb grafana
 Write-Output "--------------------------------------------------------------------------------------"
 Write-Output "Load testing with Grafana dashboard http://localhost:3000/d/k6/k6-load-testing-results"
 Write-Output "--------------------------------------------------------------------------------------"
-docker-compose run --rm k6 run /scripts/ewoks.js
+docker compose run --rm k6 run /scripts/ewoks.js
